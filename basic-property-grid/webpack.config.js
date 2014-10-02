f6c623443c3f097c0314cf3e78ab3a5a965a3759
radubrehar <radu@jslog.com>
2014-10-02 10:46:51 +0300

    part 1 for basic-property-grid

---
+++ basic-property-grid/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        publicPath: 'http://localhost:8090/assets'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+            }
+        ]
+    },
+    externals: {
+        'react': 'React'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
